2918ff0d22bd7c96f4db792052e3894ade4e05a5
Martin Ždila <m.zdila@gmail.com>
2022-09-30 15:36:29 +0200

    deleted build output

--- ttt/lib/index.d.ts
+++
@@ -1,9 +0,0 @@
-import type { Map, IControl } from 'maplibre-gl';
-export declare class GeocodingControl implements IControl {
-    #private;
-    constructor({ apiKey }: {
-        apiKey: string;
-    });
-    onAdd(map: Map): HTMLDivElement;
-    onRemove(): void;
-}
