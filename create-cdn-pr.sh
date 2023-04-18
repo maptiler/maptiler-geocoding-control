82ec21db6611547f1a6d9017a012ea800eefafbc
Martin Ždila <m.zdila@gmail.com>
2023-04-18 12:38:50 +0200

    GC-208 added script for publishing to CDN

---
+++ create-cdn-pr.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e
+
+export GIT_WORK_TREE=../cdn.maptiler.com
+export GIT_DIR=$GIT_WORK_TREE/.git
+
+git checkout main
+
+git pull
+
+VERSION=$(npm view . version)
+
+git checkout -b maptiler-geocoding-control-$VERSION
+
+BASE=../cdn.maptiler.com/maptiler-geocoding-control
+
+mkdir -p $BASE/v$VERSION
+
+cp -r dist/* $BASE/v$VERSION
+
+ln -sf v$VERSION $BASE/latest
+
+git add maptiler-geocoding-control/v$VERSION maptiler-geocoding-control/latest
+
+git reset maptiler-geocoding-control/v$VERSION/*.tgz
+
+git commit -m "Add maptiler-geocoding-control v$VERSION"
+
+gh pr create --base main --fill --repo maptiler/cdn.maptiler.com --reviewer "@petrsloup"
